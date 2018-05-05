ad7f164fa3a50ff4c175f319830dda5702897177
CristinaGonzalezAlcala25 <cristina8811.cg@gmail.com>
2018-05-05 11:11:24 +0200

    P5_Quiz

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const quizController = require('../controllers/quiz');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Quiz' });
+});
+
+//Credits page.
+router.get('/credits', function(req, res, next) {
+    res.render('credits');
+});
+
+//List of quizzes
+router.get('/quizzes', quizController.index);
+
+    module.exports = router;
