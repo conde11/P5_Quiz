dc9debfd3264a407585e64647b9097958274866b
CristinaGonzalezAlcala25 <cristina8811.cg@gmail.com>
2018-05-05 11:41:19 +0200

    soy lerda

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const Sequelize = require('sequelize');
+
 
 const sequelize = new Sequelize("sqlite:quizzes.sqlite", {logging: false});
 
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
 const quizController = require('../controllers/quiz');
 
 /* GET home page. */
